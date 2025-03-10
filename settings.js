e49fc8572763141d0089cd076ed56f38461ce80f
Vizi-n0x642 <kp2471261@gmail.com>
2025-03-10 15:01:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Vizinox', // Your name
-  ownerNumber: '9188147019', //Your number
+  ownerNumber: '919188147019', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
